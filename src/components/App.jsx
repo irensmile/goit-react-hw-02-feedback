1374e26a9e840f3b9ef2b091f1b799b24c2b9343
irensmile <curkan.ira@gmail.com>
2023-11-30 10:45:00 -0500

    feedback-step1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,62 @@
-export const App = () => {
-  return (
-    <div
+
+import React, { Component } from "react";
+
+
+export class App extends Component { 
+  state = {
+  good: 0,
+  neutral: 0,
+  bad: 0
+  }
+  
+
+
+  render() {
+    return <div
       style={{
         height: '100vh',
-        display: 'flex',
+        //display: 'flex',
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
         color: '#010101'
       }}
     >
-      React homework template
-    </div>
-  );
-};
+      <div>
+      <h2>Please leave feedback</h2>
+      <ul className="feedback">
+        <li><button type="button" onClick={this.onClick}>Good</button></li>
+        <li><button type="button" onClick={this.onClick}>Neutral</button></li>
+        <li><button type="button" onClick={this.onClick}>Bad</button></li>
+        </ul>
+      </div>
+
+      <div>
+      <h2>Statistics</h2>
+      <ul className="feedback">
+          <li>Good: <span>{ this.state.good}</span></li>
+        <li>Neutral: <span>{ this.state.neutral}</span></li>
+        <li>Bad: <span>{ this.state.bad}</span></li>
+        </ul>
+      </div>
+
+    </div>;
+  }
+  onClick = (event) => {
+    switch (event.target.innerText) {
+      case 'Good':
+        this.setState({ good: this.state.good + 1 });
+        break;
+      case 'Neutral':
+        this.setState({ neutral: this.state.neutral + 1 });
+        break;
+      case 'Bad':
+        this.setState({ bad: this.state.bad + 1 });
+        break;
+    }
+}
+
+}
+
+
+
